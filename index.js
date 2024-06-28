2fc17857fbcc22e229ad64be52850462c0de91a3
unknown <sundaygodwin007>
2024-06-28 03:39:39 -0700

    updated the contact,pastors and testimonies pages

---
+++ index.js
@@ -0,0 +1,11 @@
+alert("welcome to ChristLove Family Church of New Beginning");
+
+
+
+// const button = document.querySelector(".btn_pray");
+
+// function changeHeader(){
+//     document.querySelector("h5").textContent = "Prayer Points";
+// }
+
+// BigInt.addEventListener("click", changeHeader);
